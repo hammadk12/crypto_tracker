03fb45abdfe2c8139c912e176e9cb8ef5ee74565
hammadk12 <131798537+hammadk12@users.noreply.github.com>
2024-04-09 14:35:05 -0400

    REFINED TERMS PAGE - in Terms.js: - revised and edited styling for responsiveness - ensured design was cohesive and maintained layout - changed text color & added background container

--- src/components/Terms.js
+++ src/components/Terms.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+
 
 const Terms = () => {
 
@@ -10,55 +10,62 @@ const currentDate = new Date().toLocaleDateString("en-US", {
 });
 
   return (
-    <div className='p-10'>
+    <div className='px-10 md:px-20 lg:px-40'>
         {/* Side bar */}
-        <div className='p-10 text-center'>
+        <div className='p-10 text-center mt-[-50px]'>
             <h2 className='font-bold text-3xl md:text-4xl lg:text-5xl mb-4'>Terms & Conditions</h2>
             <p className='mb-4'>Last Updated: {currentDate}<br /></p>
         </div>
 
         {/* Main Content */}
-        <p className='text-left p-10'>This page outlines the Terms & Conditions for the use of CTrack's services. It is important to understand these terms as they govern your use of our site and services. By accessing CTrack, you agree to be bound by these conditions. Last Updated: </p>
-        <div className='p-10'>
-            <h3 className='font-bold text-lg mb-2'>Usage of Services</h3>
-        <section>
-            <h2 className='text-2xl font-semibold mt-4 mb-2'>1. Introduction</h2>
-            <p className='mb-4'>Welcome to CTrack! These terms of service outline the rules and regulations for the use of CTrack's Website and services.</p>
-        </section>
+        <div className='p-10 bg-[#FAF0E6] rounded-xl shadow-xl flex flex-col'>
+            <h3 className='font-bold text-lg mb-2 override-text-color'>Usage of Services</h3>
+         <section>
+            <h2 className='text-2xl font-semibold mt-4 mb-2 override-text-color'>1. Introduction</h2>
+            <p className='mb-4 override-text-color'>Welcome to CTrack! These terms of service outline the rules and regulations for the use of CTrack's Website and services.</p>
+         </section>
 
-        <section>
-            <h2 className='text-2xl font-semibold mt-4 mb-2'>2. Privacy Policy</h2>
-            <p className='mb-4'>Your privacy is important to us. Our Privacy Policy outlines how we collect, use, and protect your data. Please review it to understand our practices.</p>
-        </section>
+         <section>
+            <h2 className='text-2xl font-semibold mt-4 mb-2 override-text-color'>2. Privacy Policy</h2>
+            <p className='mb-4 override-text-color'>Your privacy is important to us. Our Privacy Policy outlines how we collect, use, and protect your data. Please review it to understand our practices. We use the information we collect in various ways, including to:
+                - Provide, operate, and maintain our website
+                - Improve, personalize, and expand our website
+                - Understand and analyze how you use our website
+                - Develop new products, services, features, and functionality
+                - Communicate with you, either directly or through one of our partners, including for customer service, to provide you with updates and other information relating to the website, and for marketing and promotional purposes
+                - Send you emails
+                - Find and prevent fraud
+            </p>
+         </section>
 
-        <section>
-            <h2 className="text-2xl font-semibold mt-4 mb-2">3. User Obligations</h2>
-            <p className="mb-4">
+         <section>
+            <h2 className="text-2xl font-semibold mt-4 mb-2 override-text-color">3. User Obligations</h2>
+            <p className="mb-4 override-text-color">
                 As a user of our website, you agree not to misuse the services or help anyone else do so. You are responsible for ensuring that your use of our services is in compliance with laws and regulations.
             </p>
-        </section>
+         </section>
 
-        <section>
-            <h2 className="text-2xl font-semibold mt-4 mb-2">4. Product Information</h2>
-            <p className="mb-4">
+         <section>
+            <h2 className="text-2xl font-semibold mt-4 mb-2 override-text-color">4. Product Information</h2>
+            <p className="mb-4 override-text-color">
                 We make every effort to display as accurately as possible the colors and images of our products. We cannot guarantee that your computer monitor's display of any color will be accurate.
             </p>
-        </section>
+         </section>
 
-        <section>
-            <h2 className="text-2xl font-semibold mt-4 mb-2">5. Payment and Pricing</h2>
-            <p className="mb-4">
+         <section>
+            <h2 className="text-2xl font-semibold mt-4 mb-2 override-text-color">5. Payment and Pricing</h2>
+            <p className="mb-4 override-text-color">
                 We reserve the right to refuse or cancel orders at any time. Prices for our products are subject to change without notice.
             </p>
-        </section>
+         </section>
 
-        <section>
-            <h2 className="text-2xl font-semibold mt-4 mb-2">6. Return Policy</h2>
-            <p className="mb-4">
+         <section>
+            <h2 className="text-2xl font-semibold mt-4 mb-2 override-text-color">6. Return Policy</h2>
+            <p className="mb-4 override-text-color">
                 If you have any questions feel free to 
                 <span className='text-blue-600 hover:underline cursor-pointer'> Contact</span> us.
             </p>
-        </section>
+         </section>
         </div>
     </div>
   )
