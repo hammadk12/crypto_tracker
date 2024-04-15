5b90d3823ce6d97052d5af595d5f6367712942ea
hammadk12 <131798537+hammadk12@users.noreply.github.com>
2024-04-15 08:41:35 -0400

    REFINED HEADER COMPONENT - in HomePage.js: - revamped header component to ensure consistent design - tested responsiveness

--- src/components/AvatarDemo.js
+++ src/components/AvatarDemo.js
@@ -2,8 +2,8 @@ import React from 'react';
 import * as Avatar from '@radix-ui/react-avatar';
 
 const AvatarDemo = () => (
-  <div className="flex">
-    <Avatar.Root className="bg-blackA1 inline-flex h-[45px] w-[45px] select-none items-center justify-center overflow-hidden rounded-full align-middle">
+  <div className="flex relative">
+    <Avatar.Root className="bg-blackA1 inline-flex h-[45px] w-[45px] select-none items-center justify-center overflow-hidden rounded-full align-middle z-30">
       <Avatar.Image
         className="h-full w-full rounded-[inherit] object-cover"
         src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
@@ -16,7 +16,7 @@ const AvatarDemo = () => (
         CT
       </Avatar.Fallback>
     </Avatar.Root>
-    <Avatar.Root className="bg-blackA1 inline-flex h-[45px] w-[45px] select-none items-center justify-center overflow-hidden rounded-full align-middle">
+    <Avatar.Root className="bg-blackA1 inline-flex h-[45px] w-[45px] select-none items-center justify-center overflow-hidden rounded-full align-middle z-20 -ml-3">
       <Avatar.Image
         className="h-full w-full rounded-[inherit] object-cover"
         src="https://images.unsplash.com/photo-1543610892-0b1f7e6d8ac1?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE0fHx8ZW58MHx8fHx8"
@@ -29,7 +29,7 @@ const AvatarDemo = () => (
         JD
       </Avatar.Fallback>
     </Avatar.Root>
-    <Avatar.Root className="bg-blackA1 inline-flex h-[45px] w-[45px] select-none items-center justify-center overflow-hidden rounded-full align-middle">
+    <Avatar.Root className="bg-blackA1 inline-flex h-[45px] w-[45px] select-none items-center justify-center overflow-hidden rounded-full align-middle z-10 -ml-3">
       <Avatar.Image
         className="h-full w-full rounded-[inherit] object-cover"
         src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fGhlYWRzaG90JTIwY2lyY3VsYXIlMjBwbmd8ZW58MHx8MHx8fDA%3D"
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -4,19 +4,18 @@ import Testimonial from './Testimonial'
 import Recognition from './Recognition'
 import Faq from './Faq'
 import AvatarDemo from './AvatarDemo'
-import FiveStars from './FiveStars'
 
 const HomePage = () => {
   return (
     <div>
-        <header className='flex flex-col justify-center items-center mb-[290px] mt-18'>
-          <h2 className='font-bold text-center mt-20 text-4xl md:text-5xl lg:text-6xl p-10'>CTrack - Start Tracking Today</h2>
-          <p className='mb-5 text-lg md:text-xl lg:text-2xl text-center p-5'>Join millions of other users taking advantage of this powerful tracking tool!</p>
-          <button style={{ borderColor: '#FAF0E6'}} className='border px-6 py-3'>Explore</button>
-        <div className='flex items-center gap-10 mt-10'>
-          <AvatarDemo />
-          <FiveStars />
+        <header className='flex flex-col justify-center items-center mb-[290px] mt-12 bg-[#FAF0E6] rounded-xl mx-14 lg:mx-[200px] p-8'>
+          <h2 className='font-bold text-center mt-20 text-4xl md:text-5xl lg:text-7xl override-text-color'>CTrack - Start Tracking Today</h2>
+          <p className='text-xl md:text-2xl text-center my-12 override-text-color'>Join <span className='text-indigo-600 font-bold'>millions</span> of other users taking advantage of this powerful tracking tool!</p>
+        <div className='flex flex-col md:flex-row items-center gap-10 mb-10 md:mx-12'>
+          <AvatarDemo/>
+          <p className='text-lg override-text-color text-center'>"CTrack has <span className='text-indigo-600 font-bold'>revolutionized</span> the way I monitor my crypto investments."</p>
         </div>
+        <button className='bg-gray-800 text-white px-5 md:px-8 py-2.5 md:py-5 rounded-lg hover:bg-black transition ease-in duration-150 mb-14'>Explore</button>
         </header>
         <Features />
         <Testimonial />
